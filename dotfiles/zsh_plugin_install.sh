0d67cf60cc616fd6b46685a0f7247070b96609ea
haranrk <haranrajkumar97@gmail.com>
2019-04-10 21:45:54 +0530

    Fix bug in zsh_plugin_install

--- dotfiles/zsh_plugin_install.sh
+++ dotfiles/zsh_plugin_install.sh
@@ -8,5 +8,6 @@ bundle() {
 bundle 'zsh-users/zsh-autosuggestions'
 bundle 'zsh-users/zsh-syntax-highlighting' 
 bundle 'MichaelAquilina/zsh-you-should-use'
+mv ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-you-should-use ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/you-should-use
 bundle 'oldratlee/hacker-quotes'
 
