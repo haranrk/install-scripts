47f9e6559c532d3bb713cdf95577aad66c5b8f54
haranrk <haranrajkumar97@gmail.com>
2019-09-20 13:14:30 +0530

    Add setup docker file

---
+++ setups/setup_docker.sh
@@ -0,0 +1,17 @@
+#Ubuntu 16.04 & 18.04
+
+#Install docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#Install nvidia container runtime
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update 
+sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
