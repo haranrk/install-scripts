c7edca5fb6a5e0293d01b6971551d63a8a4d1e6c
haranrk <haranrajkumar97@gmail.com>
2019-10-03 12:23:47 +0530

    Prettify code

--- setups/setup_latest_tmux.sh
+++ setups/setup_latest_tmux.sh
@@ -1,18 +1,12 @@
+#Builds and installs tmux 2.9a
+
 sudo apt update
 sudo apt install -y git automake build-essential pkg-config libevent-dev libncurses5-dev byacc
-
 git clone https://github.com/tmux/tmux.git temp-tmux
-
 cd temp-tmux
-
 git checkout tags/2.9a
-
 sh autogen.sh
-
 ./configure && make
-
 sudo make install
-
 cd -
-
 rm -fr temp-tmux
