9384f35b7dcb2adbce4d12b12d57dd0c0106bd19
haranrk <haranrajkumar97@gmail.com>
2019-10-06 10:19:23 +0530

    Add func to install custom zsh themes

--- setups/zsh_plugin_install.sh
+++ setups/zsh_plugin_install.sh
@@ -5,8 +5,17 @@ omz_bundle() {
 	git clone https://github.com/${STR[0]}/${STR[1]} ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/${STR[1]}
 }
 
+omz_bundle_theme() {
+	IFS="/"
+	read -ra STR <<< "$1" 
+	IFS=" "
+	git clone https://github.com/${STR[0]}/${STR[1]} ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/${STR[1]}
+}
+
 omz_bundle 'zsh-users/zsh-autosuggestions'
 omz_bundle 'zdharma/fast-syntax-highlighting'
 omz_bundle 'Valiev/almostontop'
 omz_bundle 'esc/conda-zsh-completion'
+#omz_bundle 'denysdovhan/spaceship-prompt'
+omz_bundle_theme 'romkatv/powerlevel10k'
 
