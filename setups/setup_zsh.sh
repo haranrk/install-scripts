789a6de13f6e5017b857c6e54ac0ef807a8d6b84
haranrk <haranrajkumar97@gmail.com>
2021-03-10 12:40:30 +0530

    Add zshrc init lines

--- setups/setup_zsh.sh
+++ setups/setup_zsh.sh
@@ -6,6 +6,20 @@ elif [ $OSTYPE = "linux-gnu" ]; then
 	echo "Detected linux"
 	sudo apt install zsh
 fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+
+#Create zshrc if it does not exist 
+if [[ ! -e ~/.zshrc ]]; then
+	echo "Zshrc not present, creating it"
+	touch ~/.zshrc
+fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cat ${DIR}/assets/zshrc_init.sh >> ~/.zshrc 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+if [[ $OSTYPE = "darwin"* ]]; then
+	echo "Detected macos, running compaudit"
+	compaudit | xargs chmod g-w,o-w	
+fi
 bash ./setups/zsh_plugin_install.sh
 
