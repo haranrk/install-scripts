6317b92106c001295068951d3b0909855301ac0d
haranrk <haranrajkumar97@gmail.com>
2021-03-10 13:03:25 +0530

    Add helper instructions

--- setups/setup_git.sh
+++ setups/setup_git.sh
@@ -1,6 +1,8 @@
+echo "Setting up git"
 git config --global color.ui true
 git config --global user.name "haranrk"
 git config --global user.email "haranrajkumar97@gmail.com"
 
 ssh-keygen -t rsa -b 4096 -C "haranrajkumar97@gmail.com"
 cat ~/.ssh/id_rsa.pub
+echo "Copy the above ssh key and paste it at https://github.com/settings/keys"
