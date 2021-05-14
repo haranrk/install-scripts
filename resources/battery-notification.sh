03949551ac44c2d2af67b15af6071cd92eca68f6
haranrk <rajkumar.haran@moneyforward.co.jp>
2021-05-14 12:28:35 +0530

    Add track charging to battery-notifier

--- resources/battery-notification.sh
+++ resources/battery-notification.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 percent=$(system_profiler SPPowerDataType | grep "State of Charge (%)" | awk '{print $5}')
+charging=$(system_profiler SPPowerDataType | grep -m 1 "Charging:" | awk '{print $2}')
+echo "Battery Level $percent | Charging? $charging"
 
-if [[ $percent > 75 ]] 
+if [[ $percent > 75 ]] && [[ $charging == "Yes" ]]
 then
     osascript -e "display notification \"Battery Level $percent\" with title \"Battery Overcharged\""
-elif [[ $percent < 25 ]] 
+elif [[ $percent < 25 ]] && [[ $charging == "No" ]] 
 then
     osascript -e "display notification \"Battery Level $percent\" with title \"Battery Undercharged\""
 fi
