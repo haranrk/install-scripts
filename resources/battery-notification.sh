e3cdd39c4b4715fc5d366d84184b5c7f96691ba7
haranrk <rajkumar.haran@moneyforward.co.jp>
2021-05-14 14:49:56 +0530

    Update

--- resources/battery-notification.sh
+++ resources/battery-notification.sh
@@ -3,10 +3,10 @@ percent=$(system_profiler SPPowerDataType | grep "State of Charge (%)" | awk '{p
 charging=$(system_profiler SPPowerDataType | grep -m 1 "Charging:" | awk '{print $2}')
 echo "Battery Level $percent | Charging? $charging"
 
-if [[ $percent > 75 ]] && [[ $charging == "Yes" ]]
+if [[ $percent > 74 ]] && [[ $charging == "Yes" ]]
 then
     osascript -e "display notification \"Battery Level $percent\" with title \"Battery Overcharged\""
-elif [[ $percent < 25 ]] && [[ $charging == "No" ]] 
+elif [[ $percent < 26 ]] && [[ $charging == "No" ]] 
 then
     osascript -e "display notification \"Battery Level $percent\" with title \"Battery Undercharged\""
 fi
